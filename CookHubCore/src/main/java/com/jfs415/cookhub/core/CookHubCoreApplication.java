c54aa569feac72e9e290a90bcf6c148ab6c5f885
Jon Healy <jhealy278@gmail.com>
2024-05-14 09:01:55 -0400

    feat(core)!: Initial Core Files
    BREAKING-CHANGE: Gradle Update, application.yml update

---
+++ CookHubCore/src/main/java/com/jfs415/cookhub/core/CookHubCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jfs415.cookhub.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookHubCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookHubCoreApplication.class, args);
+	}
+
+}
---
+++ CookHubCore/src/test/java/com/jfs415/cookhub/core/CookHubCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfs415.cookhub.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookHubCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
